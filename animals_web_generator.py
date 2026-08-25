f4e29648ac8e926001fea4aede8fb7fb1fe755d0
dni-schaf <d.scharffenberg@web.de>
2026-08-25 15:27:05 +0200

    add read replace and write html function

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,24 +2,45 @@ import json
 
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """Loads a JSON file"""
+    with open(file_path, "r", encoding="utf-8") as handle:
+        return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def animals_output(data):
+    output = ""
+    for animal in data:
+        if name := animal.get("name"):
+            output += f"Name: {name}\n"
 
-def print_animals(data):
-    for animal in animals_data:
-        if name := animal.get('name'):
-            print(f"Name: {name}")
-        if diet := animal.get('characteristics').get('diet'):
-            print(f"Diet: {diet}")
-        if location := animal.get('locations')[0]:
-            print(f"Location: {location}")
-        if animal_type := animal.get('characteristics').get('type'):
-            print(f"Type: {animal_type}")
-        print()
+        # Sicheres Auslesen verschachtelter Dictionaries:
+        characteristics = animal.get("characteristics", {})
+        if diet := characteristics.get("diet"):
+            output += f"Diet: {diet}\n"
+
+        locations = animal.get("locations", [])
+        if locations:
+            output += f"Location: {locations[0]}\n"
+
+        if animal_type := characteristics.get("type"):
+            output += f"Type: {animal_type}\n"
+
+        output += "\n"
+    return output
 
 
-print_animals(animals_data)
+# Hauptprogramm
+animals_data = load_data("animals_data.json")
+
+with open("animals_template.html", "r", encoding="utf-8") as file:
+    html_template = file.read()
+
+# Erzeugen und Ersetzen
+animals_info = animals_output(animals_data)
+new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
+
+# Ausgabe des finalen HTML-Codes
+print(new_html)
+
+with open("animals.html", "w", encoding="utf-8") as file:
+    file.write(new_html)
