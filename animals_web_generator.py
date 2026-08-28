49d8c75d28b0b2e5cbf8c120f66113de75ef61d5
dni-schaf <d.scharffenberg@web.de>
2026-08-28 17:42:54 +0200

    update cards design

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -6,43 +6,48 @@ def load_data(file_path):
     with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
-
-def animals_output(data):
+def serialize_animal(animal):
     output = ""
+    output += '<li class="cards__item">'
+    if name := animal.get("name"):
+        output += f'<div class="card__title"> {name}</div>'
+    output += '<div class="card__text">'
+    output += '<ul class="list">'
 
-    for animal in data:
-        output += '<li class="cards__item">'
-        if name := animal.get("name"):
-            output += f'<div class="card__title"> {name}</div>'
-        output += '<p class="card__text">'
+    characteristics = animal.get("characteristics", {})
+    if diet := characteristics.get("diet"):
+        output += f"<li><strong>Diet:</strong> {diet}</li>"
 
-        characteristics = animal.get("characteristics", {})
-        if diet := characteristics.get("diet"):
-            output += f"<strong>Diet:</strong> {diet}<br/>"
+    locations = animal.get("locations", [])
+    if locations:
+        output += f"<li><strong>Location:</strong> {locations[0]}</li>"
 
-        locations = animal.get("locations", [])
-        if locations:
-            output += f"<strong>Location:</strong> {locations[0]}<br/>"
-
-        if animal_type := characteristics.get("type"):
-            output += f"<strong>Type:</strong> {animal_type}<br/>"
-        output += '</p>'
-        output += '</li>'
+    if animal_type := characteristics.get("type"):
+        output += f"<li><strong>Type:</strong> {animal_type}</li>"
+    output += '</ul>'
+    output += '</div>'
+    output += '</li>'
     return output
 
 
-# Hauptprogramm
+def animals_output(data):
+    output = ''
+    for animal in data:
+        output += serialize_animal(animal)
+    return output
+
+
 animals_data = load_data("animals_data.json")
+
 
 with open("animals_template.html", "r", encoding="utf-8") as file:
     html_template = file.read()
 
-# Erzeugen und Ersetzen
+
 animals_info = animals_output(animals_data)
 new_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
 
-# Ausgabe des finalen HTML-Codes
-print(new_html)
 
 with open("animals.html", "w", encoding="utf-8") as file:
     file.write(new_html)
+
